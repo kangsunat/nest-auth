25b05a4a3744ce6cefc40ae8495910723f3eb099
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-08-02 17:48:49 +0700

    generate resourse auth and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
+import { CobaModule } from './coba/coba.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, CobaModule],
 })
 export class AppModule {}
