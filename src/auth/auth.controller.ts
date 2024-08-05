c92e32e367377b5bc266e6800375a8fac7ec16ae
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-08-05 15:46:31 +0700

    create jwt sign

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Post('login')
   login(@Body() body: LoginDto) {
-    return true;
+    return this.authService.validateUser(body);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
   private readonly fakeUser = [
     { id: 1, username: 'nama username', password: 'pass1' },
   ];
+
   validateUser(body: LoginDto) {
     const getUser = this.fakeUser.find(
       (user) => user.username == body.username,
     );
 
-    if (getUser && getUser.password === body.password) return getUser;
+    if (getUser && getUser.password === body.password) {
+      const { password, ...result } = getUser;
+      return this.jwtService.sign(result);
+    }
 
     throw new UnauthorizedException();
   }
