663c774e7d8f76cd4716d66e146f9ee9cc89b1c8
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-08-06 10:00:16 +0700

    delete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [],
 })
 export class AppModule {}
